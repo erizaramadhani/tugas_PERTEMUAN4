6a463d33fefeebec14feb442fdea72af747d6581
erizaramadhani <erizarmdr28@gmail.com>
2022-04-07 06:22:29 +0700

    menambahkan application/controllers/web.php, application/views/v_header.php, application/views/v_footer.php, application/views/v_index.php, sytems/assets/css, application/config/config.php, application/views/v_about.php

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+     <h1><?php echo $judul ?></h1>
+     <h4>Nama</h4> 
+     <ul type="disc">
+          <li>Nama Depan : Eriza Ramadhani</li> 
+          <li>Nama Belakang : Rusiadi</li>
+         </ul> 
+         <br> 
+         <h4>Alamat</h4> 
+         <ul type="none"> 
+             <li> Bukit cengkeh 2 blok f8 no5</li>
+             </ul> 
+             
+             <h4>Tempat Lahir</h4>
+              <ul type="none"> 
+                  <li>Depok</li> 
+                </ul> 
+                
+                <h4>NIM dan Kelas</h4>
+                 <ul type="square">
+                      <li>NIM : 17190983</li>
+                       <li>Kelas : 17.6E.01</li>
+                     </ul>
+                     </section>
